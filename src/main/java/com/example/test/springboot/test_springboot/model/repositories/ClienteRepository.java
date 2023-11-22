65af1ce058e6a73bb7500642e32b72e7ac456f66
jvscJoao <joaov1500@gmail.com>
2023-11-22 16:26:09 -0300

    Criando ClienteRepository

--- src/main/java/com/example/test/springboot/test_springboot/model/repositories/ClienteRepository.java
+++ src/main/java/com/example/test/springboot/test_springboot/model/repositories/ClienteRepository.java
@@ -1,9 +1,28 @@
 package com.example.test.springboot.test_springboot.model.repositories;
 
-import org.springframework.data.repository.Repository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.test.springboot.test_springboot.model.entities.Cliente;
 
-public interface ClienteRepository extends Repository<Cliente, Integer> {
-    
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
+    /*
+     * Repository: É usado para armazenamento de dados, no caso estou armazenando Cliente(s)
+     * CrudRepository: Interface que fornece métodos para realizar operações CRUD (Create, Read, Update, Delete)
+     * Primeiro parâmetro: Entidade/Classe fornecisa
+     * Segundo parâmetro: Tipo do identificador da entidade, no caso Integer
+     */
+
+    /*
+     * Query Creation
+     * É usado para criar consultas no BD usando nomeclatura nos métodos
+     * Documentação: https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+     */
+
+    //Procurar por Clientes com nome parecido passado no parâmetro
+    public Optional<List<Cliente>> findByNomeLikeOrderByNome(String nome);
 }
