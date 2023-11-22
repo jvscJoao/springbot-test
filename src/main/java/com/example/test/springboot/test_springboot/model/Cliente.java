d9cf4a1199b9d2afdabde6c99ef572d4fb27806b
jvscJoao <joaov1500@gmail.com>
2023-11-21 21:29:26 -0300

    Criando classe Cliente

---
+++ src/main/java/com/example/test/springboot/test_springboot/model/Cliente.java
@@ -0,0 +1,65 @@
+package com.example.test.springboot.test_springboot.model;
+
+public class Cliente {
+    
+    
+    private int id;
+    private String nome;
+    private String cpf;
+
+    Cliente() {}
+
+    Cliente(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
