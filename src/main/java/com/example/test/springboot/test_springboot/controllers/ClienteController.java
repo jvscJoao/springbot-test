9dbb2fafd179ef6d7d120bb749928c0da9ecbdc4
jvscJoao <joaov1500@gmail.com>
2023-11-24 15:40:34 -0300

    Delete Cliente

--- src/main/java/com/example/test/springboot/test_springboot/controllers/ClienteController.java
+++ src/main/java/com/example/test/springboot/test_springboot/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.example.test.springboot.test_springboot.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,8 @@ public class ClienteController {
         return clienteRepository.findById(id);
     }
 
-
+    @DeleteMapping("/{id}")
+    public void apagarCliente(@PathVariable int id) {
+        clienteRepository.deleteById(id);
+    }
 }
