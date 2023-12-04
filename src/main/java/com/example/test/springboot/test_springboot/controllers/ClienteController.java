46497612a2b82a8f69edd7a5b6bcc73a507f2406
jvscJoao <joaov1500@gmail.com>
2023-12-04 12:20:46 -0300

    Adicionando responseentity para metodo post

--- src/main/java/com/example/test/springboot/test_springboot/controllers/ClienteController.java
+++ src/main/java/com/example/test/springboot/test_springboot/controllers/ClienteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,9 +51,9 @@ public class ClienteController {
      * @ResponseBody: Interface usava para adicionar entidades e facilmente percorrer.
     */
     @PostMapping
-    public @ResponseBody Cliente create(Cliente cliente) {
-        clienteRepository.save(cliente);
-        return cliente;
+    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+        Cliente savedCliente = clienteRepository.save(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCliente);
     }
 
     /*
