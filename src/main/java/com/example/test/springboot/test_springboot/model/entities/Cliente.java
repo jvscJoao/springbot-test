319e0fef0f92a8562f130ff02f145e65539de3ea
jvscJoao <joaov1500@gmail.com>
2023-12-05 17:32:42 -0300

    Usando Lomobk na entidade Cliente

--- src/main/java/com/example/test/springboot/test_springboot/model/entities/Cliente.java
+++ src/main/java/com/example/test/springboot/test_springboot/model/entities/Cliente.java
@@ -1,79 +1,45 @@
 package com.example.test.springboot.test_springboot.model.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity // Usada para facilitar mapeamento da classe para o Banco de Dados
+@Entity // é usada para indicar que a classe é uma entidade JPA, mapeada para uma tabela no banco de dados.
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+//Getter, Setter, AllArgsConstructor, NoArgsConstructor, EqualsAndHashCode: gera automaticamente os métodos
+
 public class Cliente {
-    
-    @Id // Identificar chave primária para uma tabela do banco de dados
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    /*
-     * Define como as chaves primárias devem ser geradas
-     * GenerationType.AUTO: JPA escolhe automaticamente a estratégia apropriada.
-     * GenerationType.IDENTITY: Autoincremento no banco de dados.
-     * GenerationType.SEQUENCE: Gerador de sequência no BD.
-     * GenerationType.TABLE: Usa uma tabela de BD para gerar chaves.
-    */
     private int id;
+    @NotBlank
     private String nome;
+    @NotBlank
+    @Column(unique = true)
     private String cpf;
 
-    public Cliente() {}
-
-    public Cliente(int id, String nome, String cpf) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return this.cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Cliente other = (Cliente) obj;
-        if (id != other.id)
-            return false;
-        return true;
-    }
-
-    
 }
+
+/*
+    * Id: Identificar chave primária para uma tabela do banco de dados
+    * GeneratedValue: Define como as chaves primárias devem ser geradas
+        * GenerationType.AUTO: JPA escolhe automaticamente a estratégia apropriada.
+        * GenerationType.IDENTITY: Autoincremento no banco de dados.
+        * GenerationType.SEQUENCE: Gerador de sequência no BD.
+        * GenerationType.TABLE: Usa uma tabela de BD para gerar chaves.
+    *
+    * NotBlank: variável não pode ser vazio
+    * Column - unique: valor unico na tabela
+*/
